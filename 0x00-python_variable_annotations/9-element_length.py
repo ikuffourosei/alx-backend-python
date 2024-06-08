8368803d4d4ac7266fe2a3d4b2ce12bd0609ce34
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-08 21:15:27 +0000

    Python3 Annotations

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,5 +2,8 @@
 """Annotating the function below"""
 
 from typing import List, Tuple, Iterable, Sequence
+
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''Annotate to display return type'''
     return [(i, len(i)) for i in lst]
