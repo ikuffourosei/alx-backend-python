17230d8a4e4d7e292c3b31fb58c0c2d0d1a6a8fc
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-08 21:14:01 +0000

    Python3 Annotations

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Annotating the function below"""
+
+from typing import List, Tuple, Iterable, Sequence
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
