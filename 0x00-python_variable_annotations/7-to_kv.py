e2c1b8c86867d8d5fc1fe166ee9a64881909acbf
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-08 20:55:15 +0000

    Python3 Annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,8 @@ is the square of the int/float v and should be annotated as a float.
 
 
 from typing import Union, Tuple
+
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple with the string k and the square of v."""
     return k, v ** 2
-
