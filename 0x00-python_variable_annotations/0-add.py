8e7c6df14e69564b0a4a59887e190e6f1526eef7
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-08 19:04:14 +0000

    Python3 Annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
+
 def add(a: float, b: float) -> float:
     """Takes two float values and returns the sum"""
     return a + b
-
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings"""
+    return str1 + str2
